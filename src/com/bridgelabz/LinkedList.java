74b724fc9fdcd3a6d1d9a126bca772d5dd57d2ef
swatikanishu <nishuswatika@gmail.com>
2022-07-01 00:08:07 +0530

    Added: link list program added

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+public class LinkedList {
+    Node head, tail;
+
+    public Node push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;//56=>30=>70
+            this.head = newNode;
+            newNode.next = temp;
+        }
+        return newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.key + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Main {
+    public static void main(String[] args){System.out.println("Welcome to linkedList DataStructure Program");
+        Operation.addDataAtStart();
+
+    }
+}
